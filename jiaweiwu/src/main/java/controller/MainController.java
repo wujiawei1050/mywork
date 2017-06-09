a5f47c66fbe73ce3af2067d08882bae895e1d214
wjw <10501715@qq.com>
2017-06-10 00:17:53 +0800

    项目上传

---
+++ jiaweiwu/src/main/java/controller/MainController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	@RequestMapping("/hi")
+	@ResponseBody
+	public String go(){
+		return "你是傻逼";
+	}
+}
