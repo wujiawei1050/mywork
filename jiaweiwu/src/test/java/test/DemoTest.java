0cabf5f87de921632fc57b8c0385e325987d24d9
jiawei.wu <w10501715@163.com>
2018-04-05 19:14:19 +0800

    junit add

---
+++ jiaweiwu/src/test/java/test/DemoTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration(locations={"classpath:conf/spring-mvc.xml"})
+public class DemoTest {
+	@Test
+	public void demo() {
+		
+	}
+}
